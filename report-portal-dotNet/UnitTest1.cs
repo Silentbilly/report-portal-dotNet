71e40b0eca4b0c978be53bce1374dac340654e55
Alexander_Kononov1@epam.com <alexander_kononov1@epam.com>
2023-03-17 22:13:53 +0300

    Add project files.

---
+++ report-portal-dotNet/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace report_portal_dotNet
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
