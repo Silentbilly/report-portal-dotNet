a8396eaeac953df77aa5b0c983f9e37ecff037c7
Alexander_Kononov1@epam.com <alexander_kononov1@epam.com>
2023-03-19 01:50:15 +0300

    Added strucutre. Added DriverSingleton, Helpers, Guard with exceptions, Driver decorator, configs

---
+++ report-portal-dotNet/UI/SeleniumHelpers/DriverSingleton.cs
@@ -0,0 +1,79 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Edge;
+using report_portal_dotNet.UI.Utils;
+
+namespace report_portal_dotNet.UI.SeleniumHelpers
+{
+    public enum DriverToUse
+    {
+        Chrome,
+        Edge,
+        Firefox
+    }
+    public class DriverSingleton
+    {
+        private static FirefoxOptions FirefoxOptions
+        {
+            get
+            {
+                var firefoxProfile = new FirefoxOptions();
+                firefoxProfile.SetPreference("network.automatic-ntlm-auth.trusted-uris", "http://localhost");
+                return firefoxProfile;
+            }
+        }
+
+        private static readonly object padlock = new();
+        private static WebDriver instance;
+        private IWebDriver driver;
+
+        public DriverSingleton()
+        {
+
+        }
+    
+
+        /*   
+         *        This thread-safe implementation uses the `lock` keyword to ensure that only one thread can access the `Instance` property at a time.
+         *        This guarantees that only one instance of the class will be created, even if multiple threads try to access it simultaneously. 
+         *        The `padlock` object is used to synchronize access to the `Instance` property, and the `lock` statement ensures that only one thread can acquire the lock and create the instance.
+         *        There is only one instance of the WebDriver class throughout the lifetime of the application. It also helps reduce the overhead of creating and destroying multiple instances of the WebDriver class.
+         */
+        public static WebDriver Instance
+        {
+            get
+            {
+                lock (padlock)
+                {
+                    var driverToUse = ConfigurationHelper.Get<DriverToUse>("DriverToUse");
+
+                    if (instance == null)
+                    {
+                        switch (driverToUse)
+                        {
+                            case DriverToUse.Chrome:
+                                instance = new ChromeDriver();
+                                break;
+                            case DriverToUse.Firefox:
+                                var firefoxProfile = FirefoxOptions;
+                                instance = new FirefoxDriver(firefoxProfile);
+                                instance.Manage().Window.Maximize();
+                                break;
+                            case DriverToUse.Edge:
+                                instance = new EdgeDriver();
+                                break;
+                            default:
+                                throw new ArgumentException("Invalid browser name.");
+                        }
+                    }
+                    return instance;
+                }
+            }
+        }
+        public void Quit()
+        {
+            instance.Quit();
+        }
+    }
+}
---
+++ report-portal-dotNet/UI/Utils/Guard.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace report_portal_dotNet.UI.Utils
+{
+    public static class Guard
+    {
+        [DebuggerStepThrough]
+        public static void IsNotNull<TExpection>(object argumentValue, string format, params object[] parameters) where TExpection : Exception
+        {
+            if (argumentValue == null)
+            {
+                throw GetException<TExpection>(format, parameters);
+            }
+        }
+
+        private static TExpection GetException<TExpection>(string format, params object[] parameters)
+        {
+            string message = string.Format(CultureInfo.InvariantCulture, format, parameters);
+            return (TExpection)Activator.CreateInstance(typeof(TExpection), message);
+        }
+
+        private static InvalidOperationException GetException(string format, params object[] parameters)
+        {
+            return new InvalidOperationException(string.Format(CultureInfo.InvariantCulture, format, parameters));
+        }
+    }
+}
